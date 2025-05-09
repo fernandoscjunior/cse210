5009f39d497ab83a2e1992ff55a199a798113846
fernandoscjunior <primeirasdezpesquisas@gmail.com>
2025-05-09 15:10:46 -0300

    Rewriting progam to learn about lists using C#

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        string digit = "-1";
+        int intDigit = int.Parse(digit);
+        List<int> numberList = new List<int>();
+        int sum = 0;
+        
+
+        while (intDigit != 0)
+        {
+            Console.Write("Enter a number:");
+            string enter = Console.ReadLine();
+            int intEnter = int.Parse(enter);
+            intDigit = intEnter;
+
+            if (intDigit == 0)
+            {
+
+            }
+            else
+            {
+                numberList.Add(intDigit);
+                sum += intDigit;
+            }
+            
+
+        }
+        int length = numberList.Count;
+        double average = sum / length;
+        int max = numberList.Max();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The biggest number is: {max}");
+
+
     }
 }
