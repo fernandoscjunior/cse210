8610505a7dea57c79941412ccf5ab0e2aeff6d32
fernandoscjunior <primeirasdezpesquisas@gmail.com>
2025-05-08 23:05:37 -0300

    Rewriting program to show grade based on percentage

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Type your grade percentage:");
+        string grade = Console.ReadLine();
+        int intGrade = int.Parse(grade);
+        string letter = "";
+
+        if (intGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (intGrade >= 80)
+        {
+            letter = "B";
+
+        }
+        else if (intGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (intGrade >= 60)
+        {
+            letter = "D";
+        }
+        else if (intGrade < 60)
+        {
+            letter = "F";
+        }
+        else 
+        {
+            Console.WriteLine("An error has occured...");
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (intGrade >= 70)
+        {
+            Console.WriteLine("Congratulations graduating on this course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry mate, try harder, better, faster, stronger next time.");
+        }
+
     }
 }
