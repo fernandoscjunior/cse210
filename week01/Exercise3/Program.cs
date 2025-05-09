d0575c06e0d5eafd028c91f03d3c5e3c5f11cff9
fernandoscjunior <primeirasdezpesquisas@gmail.com>
2025-05-09 12:52:36 -0300

    Rewriting the program to a guess the number game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,31 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {   
+        Random randomGenerator = new Random();
+        int intMagicNumber = randomGenerator.Next(1, 100);
+
+        string guessNumber = "0";
+        int intGuessNumber = int.Parse(guessNumber);
+
+        while (intGuessNumber != intMagicNumber)
+        {
+            Console.Write("What's your guess?");
+            guessNumber = Console.ReadLine();
+            intGuessNumber = int.Parse(guessNumber);
+
+            if (intGuessNumber == intMagicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (intGuessNumber > intMagicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
