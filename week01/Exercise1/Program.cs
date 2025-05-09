2ab054eca105b6cf457ea9e855e938f360068734
fernandoscjunior <primeirasdezpesquisas@gmail.com>
2025-05-08 22:28:51 -0300

    Rewriting program to include 007 reference

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What's your first name?:");
+        string first_name = Console.ReadLine();
+        Console.Write("What's your last name?:");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
