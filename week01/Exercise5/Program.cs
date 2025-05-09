d3a8de851e80bf107ddb109fe7fc8faf31def145
fernandoscjunior <primeirasdezpesquisas@gmail.com>
2025-05-09 16:59:01 -0300

    Learning how to use functions in C#

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        double squaredNumber = SquareNumber(number);
+        DisplayResult(name, squaredNumber);
+
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program.");
+
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("What's your name?: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What's your favorite number?: ");
+        string favoriteNumber = Console.ReadLine();
+        int intFavoriteNumber = int.Parse(favoriteNumber);
+        return intFavoriteNumber;
+    }
+    static double SquareNumber(int number)
+    {
+        double squaredNum = Math.Pow(number, 2);
+        return squaredNum;
+    }
+    static void DisplayResult(string name, double squaredNum)
+    {
+        Console.WriteLine($"Brother or Sister {name}, the square of your number is {squaredNum}.");
     }
 }
