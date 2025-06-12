38b9cce0e8d166a4f5801535451f1338c13498b4
fernandoscjunior <primeirasdezpesquisas@gmail.com>
2025-06-12 16:18:39 -0300

    Shape/Polymorphism activity finished

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,21 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        Circle circle = new Circle("Blue", 5);
+        Rectangle rectangle = new Rectangle("yellow", 7, 5);
+        Square square = new Square("Red", 4);
+
+        shapes.Add(square);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
